a53d1b089d2d7e56ec530b965eff22114271c147
Samandar Nemataliyev <samandarnemataliyev@gmail.com>
2024-12-19 08:42:18 +0500

    done all exercises

--- homework.py
+++ homework.py
@@ -2,7 +2,6 @@ import os, random
 
 os.system("clear")
 
-
 def random_array(n):
     return [random.randint(1, 20) for _ in range(n)]
 
@@ -11,6 +10,7 @@ o'ng qo'shnisidan kichiklarini va ularning sonini chiqaruvchi programma tuzilsin
 def func20(n):
     arr = random_array(n)
     print(arr)
+
     count = 0
     for i in range(n-1):
         if arr[i] < arr[i + 1]:
@@ -24,6 +24,7 @@ aks holda false chiqaruvchi programma tuzilsin."""
 def func21(n):
     array = random_array(n)
     arr = list(range(10, 31, 2))
+
     print(arr, sorted(arr) == arr)
     print(array, sorted(array) == array)
 
@@ -31,7 +32,24 @@ def func21(n):
 """ Series22 -> n natural soni va n ta haqiqiy son berilgan. Bu sonlar kamayish tartibida bo'lsa - 0,
 aks holda qonuniyatni buzgan element nomerini chiqaruvchi programma tuzilsin."""
 def func22(n):
-    pass
+    array = random_array(n)
+    arr = list(range(9, 31, 3))[::-1]
+
+    if sorted(arr, reverse=True) == arr:
+        print(f"{arr} kamayish tartibida: {0}")
+    else:
+        for i in range(n - 1):
+            if arr[i] < arr[i + 1]:
+                print(f"{arr} dagi qonuniyatni buzgan son indeksi {i}")
+                break
+
+    if sorted(array) == array:
+        print(f"{array} kamayish tartibida: {0}")
+    else:
+        for i in range(n - 1):
+            if array[i] < array[i + 1]:
+                print(f"{array} dagi qonuniyatni buzgan son indeks: {i}")
+                break
 
 
 """ Series23 -> n natural soni va n ta haqiqiy son berilgan. Bu sonlar arra shaklida bo'lsa - 0,
@@ -39,40 +57,66 @@ aks holda qonuniyatni buzgan element tartib raqamini chiqaruvchi programma tuzil
 Agar to'plamning har bir ichki elementi ikkala qo'shnisidan katta yoki kichik bo'lsa,
 arra shaklida bo'ladi. Masalan: (2, 3, 1, 5, 4) yoki (5, 3, 4, 2, 6)"""
 def func23(n):
-    pass
+    array = random_array(n)
+    
+    for i in range(1, n - 1):
+        if not((array[i] < array[i + 1] and array[i] < array[i - 1]) or (array[i] > array[i + 1] and array[i] > array[i - 1])):
+            return print(f"{array} dagi arrasimonlik qonuniyatini buzgan element indeksi: {i}")
+    print(f"{array} to'plam arrasimon: {0}")   
 
 
 """ Series24 -> n natural soni va n ta haqiqiy son berilgan. Bu sonlar orasidan kamida 2 ta nol bor.
 Oxirgi 2 ta nol orasidagi sonlar yig'indisini chiqaruvchi programma tuzilsin."""
-def func24(n):
-    pass
+def func24(arr):
+    array = arr[::-1]
+    first_zero = array.index(0)
+    last_zero = array.index(0, first_zero + 1)
+    print(sum(array[first_zero:last_zero]))
 
 
 """ Series25 -> n natural soni va n ta haqiqiy sonlar berilgan. Bu sonlar orasida kamida 2 ta 0 bor.
 Birinchi va oxirgi nol orasidagi sonlar yig'indisini chiqaruvchi programma tuzilsin."""
-def func25(n):
-    pass
+def func25(arr):
+    first_zero = arr.index(0)
+    last_zero = -(arr[::-1].index(0) + 1)
+    print(sum(arr[first_zero:last_zero]))
 
 
 """ Series26 -> n, k natural sonlari va n ta haqiqiy son berilgan.
 Shu sonlarning k - darajasini chiqaruvchi programma tuzilsin."""
 def func26(n, k):
-    pass
+    array = random_array(n)
+    arr = list(map(lambda num:num ** k, array))
+
+    print(array)
+    print(arr)
 
 
 """ Series27 -> n natural soni va n ta haqiqiy son berilgan. Birinchi kiritilgan sonning birinchi darajasini,
 2-sonning 2-darajasini, n-kiritilgan sonning n-darajasini chiqaruvchi programma tuzilsin."""
 def func27(n):
-    pass
+    array = random_array(n)
+    print(array)
+    for i in range(1, len(array)+1):
+        print(array[i-1] ** i)
 
 
 """ Series28 -> n natural soni n ta haqiqiy son berilgan. Birinchi kiritilgan sonning n-darajasini,
 2-kiritilgan sonning (n - 1)-darajasini, n-kiritilgan sonning 1-darajasini chiqaruvchi programma tuzilsin."""
 def func28(n):
-    pass
+    array = random_array(n)
+    arr = iter(array)
+    print(array)
+    for i in range(n):
+        nxt = next(arr)
+        print(f"{nxt} ning {n - i}-darajasi: {nxt ** (n - i)}")
 
 
 """ Series29 -> n, k natural sonlari va n ta butun sondan iborat k ta to'plam berilgan.
 Barcha sonlar yig'indisini chiqaruvchi programma tuzilsin."""
 def func29(n, k):
-    pass
+    array = [random_array(n) for _ in range(k)]
+    total = sum([sum(arr) for arr in array])
+    
+    print(array)
+    print(f"To'plamdagi barcha sonlar yig'indisi: {total}")
--- testing.py
+++ testing.py
@@ -1,31 +1,31 @@
 import homework as hw
 
-# hw.func20(10)
-# print()
+hw.func20(10)
+print()
 
-# hw.func21(10)
-# print()
+hw.func21(10)
+print()
 
 hw.func22(10)
 print()
 
-# hw.func23(10)
+hw.func23(8)
+print()
+
+hw.func24([2, 3, 5, 0, 6, 10, 13, 0, 14, 16, 20, 0, 12, 17])
+print()
+
+hw.func25([2, 3, 5, 0, 6, 10, 13, 0, 14, 16, 20, 0, 12, 17])
 # print()
 
-# hw.func24(10)
-# print()
+hw.func26(6, 3)
+print()
 
-# hw.func25(10)
-# print()
+hw.func27(5)
+print()
 
-# hw.func26(10, 3)
-# print()
+hw.func28(5)
+print()
 
-# hw.func27(10)
-# print()
-
-# hw.func28(10)
-# print()
-
-# hw.func29(10, 3)
-# print()
+hw.func29(10, 3)
+print()
